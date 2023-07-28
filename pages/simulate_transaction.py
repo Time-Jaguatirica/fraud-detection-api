c340f8b54cb02278e40d65d6d5767aea0c024d8d
jpwakugawa <jp_wakugawa@outlook.com>
2023-07-27 22:30:30 -0400

    add feature importance graph

--- pages/simulate_transaction.py
+++ pages/simulate_transaction.py
@@ -3,6 +3,8 @@ from services import S3Manager
 import dash
 from dash import html, dcc, callback, Input, Output, dash_table
 from random import randint
+import plotly.express as px
+import pandas as pd
 
 
 dash.register_page(__name__, path='/')
@@ -11,24 +13,29 @@ s3_manager = S3Manager()
 df = s3_manager.load_dataframe()
 rf_model = s3_manager.load_model()
 
+feature_importances = pd.Series(rf_model.feature_importances_, index=df.drop(columns=['Class']).columns).sort_values(ascending=False)
+feature_graph = px.bar(feature_importances)
+
 features = ['Time', 'Amount', 'V2', 'V4', 'V11', 'V10', 'V12', 'V14']
 
 layout = html.Div(className='simulate-transaction-page',
     children=[
-    html.H1(children='Simulação de Transação', style={"text-align": "center"}),
+    html.H1(children='Aplicação Prática', style={"text-align": "center"}),
 
     html.Div(className='text-box', children=[
-        html.P(children='Simulação utilizando o algoritmo Random Forest para classificar a transação em fraude ou legítima.'),
-        html.P(children='Em resumo, o Random Forest irá criar múltiplas árvores de decisão de maneira aleatória, formando o que podemos enxergar como uma floresta, onde cada árvore será utilizada na escolha do resultado final, em uma espécie de votação.'),
+        html.P(children='Simulação de insights sendo gerados em tempo real pelo algoritmo Random Forest para classificar as transações em seu négocio, podendo captar padrões e identificar fraudes imediatamente.'),
     ]),
 
-    html.H2(children='Transações em tempo real:'),
+    html.H3(children='Features de maior relevância para o algoritmo'),
+    dcc.Graph(id='feature-graph', figure=feature_graph),
+
+    html.H3(children='Transações em tempo real:'),
     dash_table.DataTable(id='live-table'),
     dcc.Interval(id='refresh', interval=10000, n_intervals=0),
 
     html.Hr(),
 
-    html.H2(children='Filtrar por:'),
+    html.H3(children='Filtrar por:'),
     dcc.RadioItems(options=['Legítima', 'Fraude'], value='Legítima', id='controls-and-radio-item'),
     dash_table.DataTable(id='filter-table'),
 
@@ -39,10 +46,9 @@ layout = html.Div(className='simulate-transaction-page',
 @callback(Output('subset', 'data'),
           Input('refresh', 'n_intervals'))
 def generate_subset_dict(n):
-    actual = randint(1, 100)
-    subset = df.loc[actual:actual+10].drop(columns=['Class'])
+    subset = generate_random_subset(df).drop(columns=['Class'])
     predictions = rf_model.predict(subset)
-    filter_subset = subset.loc[actual:actual+10, features]
+    filter_subset = subset.loc[:, features]
 
     return { "records": filter_subset.to_dict('records'), "predictions": predictions}
 
@@ -52,23 +58,28 @@ def update_table(subset_dict):
     return subset_dict["records"]
 
 @callback(Output('filter-table', 'data'),
-          Input('subset', 'data'),
           Input('controls-and-radio-item', 'value'))
-def filter_table(data, type):
-    records = data['records']
-    predictions = data['predictions']
+def filter_table(value):
+    if value == 'Fraude':
+        subset = df.loc[df['Class'] == 1]
+        subset = generate_random_subset(subset)
 
-    fraud = []
-    legitimate = []
-
-    for i in range(len(records)):
-        if predictions[i] == 0:
-            legitimate.append(records[i])
-        else:
-            fraud.append(records[i])
+    else:
+        subset = generate_random_subset(df)
+    
+    return subset.loc[:, features].to_dict('records')
 
 
-    if type == 'Legítima':
-        return legitimate
+def generate_random_subset(df):
+    df_random_values = list(df.index.values)
 
-    return fraud
+    random_list = []
+
+    for i in range(10):
+        actual_random = df_random_values[randint(0, len(df_random_values))]
+        df_random_values.remove(actual_random)
+        random_list.append(actual_random)
+
+    subset = df.loc[random_list]
+
+    return subset
